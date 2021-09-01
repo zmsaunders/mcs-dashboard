8378e3f58989f170a7bb5cbbe5c2c228ea712a2e
Zach Saunders <zsaunders@meiosis.io>
2021-09-01 16:14:59 -0400

    chart

---
+++ app/Http/Livewire/ReportChart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ReportChart extends Component
+{
+    public function render()
+    {
+        return view('livewire.report-chart');
+    }
+}
