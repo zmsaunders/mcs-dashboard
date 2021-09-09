9bc6f78b02f57c391c161c681362e75b8182e6a9
Zach Saunders <zsaunders@meiosis.io>
2021-09-09 10:09:25 -0400

    legend

--- resources/views/livewire/chart-title.blade.php
+++ resources/views/livewire/chart-title.blade.php
@@ -1,5 +1,6 @@
 <div>
     <h2 class="text-center text-2xl font-bold py-2 dark:text-gray-100">
     {{$timeTitle}}</h2>
-    <p class="text-sm text-center text-gray-500 dark:text-gray-300 pb-3">{{$schoolTitle}}</p>
+    <p class="text-sm text-center text-gray-500 dark:text-gray-300 pb-1">{{$schoolTitle}}</p>
+    <p class="text-xs text-center text-gray-400 pb-3">Click on legend to show or hide datasets</p>
 </div>
--- resources/views/livewire/chart-week-settings.blade.php
+++ resources/views/livewire/chart-week-settings.blade.php
@@ -13,5 +13,8 @@
         <a class="text-sm cursor-pointer inline-block p-1 px-3 rounded border-2 border-indigo-700
             {{$weeks == 52 ? 'bg-white dark:text-indigo-100 dark:bg-gray-800  text-indigo-700' : 'bg-indigo-700 text-indigo-100'}}"
             wire:click="setWeek(52)">One Year</a>
+        <a class="text-sm cursor-pointer inline-block p-1 px-3 rounded border-2 border-indigo-700
+            {{$weeks == 104 ? 'bg-white dark:text-indigo-100 dark:bg-gray-800  text-indigo-700' : 'bg-indigo-700 text-indigo-100'}}"
+            wire:click="setWeek(104)">Two Years</a>
     </div>
 </div>
